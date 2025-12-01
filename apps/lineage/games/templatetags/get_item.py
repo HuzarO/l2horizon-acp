a802852a9b98c150c6cc648614a6292f01630f86
Daniel Amaral <danielamaral.f@age.pe.gov.br>
2025-11-30 22:13:52 -0300

    ajuste de caixas

--- apps/lineage/games/templatetags/get_item.py
+++ apps/lineage/games/templatetags/get_item.py
@@ -20,14 +20,23 @@ def get_item_list_json(box_type):
 
     unique_items = {bi.item for bi in box_items}
 
-    return json.dumps([
-        {
-            'name': item.name,
-            'rarity': item.rarity,
-            'rarity_display': dict(RARITY_CHOICES).get(item.rarity, item.rarity),
-            'enchant': item.enchant,
-            'image_url': item.image.url if item.image else None
-        } for item in unique_items
-    ])
+    items_data = []
+    for item in unique_items:
+        image_url = None
+        if item.image:
+            try:
+                image_url = str(item.image.url)
+            except (AttributeError, ValueError):
+                pass
+        
+        items_data.append({
+            'name': str(item.name) if item.name else '',
+            'rarity': str(item.rarity) if item.rarity else '',
+            'rarity_display': str(dict(RARITY_CHOICES).get(item.rarity, item.rarity)),
+            'enchant': int(item.enchant) if item.enchant is not None else 0,
+            'image_url': image_url
+        })
+    
+    return json.dumps(items_data, ensure_ascii=False)
 
 
